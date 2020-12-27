91ed34558931db7618edc5189e97638d6bbfde8a
ranmal <ranmaldevops@gmail.com>
2020-12-27 20:43:37 +0200

    Change number 3

--- RanMalEx.java
+++ RanMalEx.java
@@ -1,7 +1,7 @@
 public class RanMalEx{
 	public static void main(String[] args){
 		for (int i = 1; i<= 10; i++){
-			System.out.println("change number 2");
+			System.out.println("change number 3");
 		}
 	}
 }
